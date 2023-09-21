82d559a06c20b03a075e1651b42c145932bd4634
MustafaAgasiya <145117178+MustafaAgasiya@users.noreply.github.com>
2023-09-21 15:54:46 +0530

    Create calcy2.py

---
+++ calcy2.py
@@ -0,0 +1,22 @@
+side=int(input("enter the side"))
+print("area of square",side*side)
+print("perimeter of square",4*side)
+
+
+
+length=int(input("enter the lenth"))
+width=int(input("enter the width"))
+print("area of rectangle",length*width)
+print("perimeter of rectangle",2*length+width)
+
+
+
+radius=int(input("enter the radius"))
+print("circumtance of circle",2*3.14*radius)
+print("area of circle",3.14*radius)
+
+
+
+
+radius=int(input("enter the radius"))
+print("circumtance of circle",2*3.14*radius)
