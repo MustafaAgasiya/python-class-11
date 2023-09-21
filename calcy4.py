8e8bea08dec5b00903e57da7f418c3a3bb85cb0a
MustafaAgasiya <145117178+MustafaAgasiya@users.noreply.github.com>
2023-09-21 15:56:29 +0530

    Create calcy4.py

---
+++ calcy4.py
@@ -0,0 +1,23 @@
+a=int(input("enter the value a"))
+b=int(input("enter the value b"))
+print(a,b)
+c=a
+a=b
+b=c
+print(a)
+print(b)
+
+
+
+
+
+fahrenheit=int(input("enter the fahrenheit"))
+celsius=5/9*(fahrenheit-32)
+print(celsius)
+
+
+
+
+celcius=int(input("enter the celsius"))
+fahrenheit=celcius*(9/5)+32
+print(fahrenheit)
