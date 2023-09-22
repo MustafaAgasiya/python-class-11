0633756e682775528ac0c74ee4842f2e71579390
MustafaAgasiya <145117178+MustafaAgasiya@users.noreply.github.com>
2023-09-22 10:14:25 -0700

    Create calcy5.py

---
+++ calcy5.py
@@ -0,0 +1,58 @@
+# Ask the user for an integer and print it with a period (.) at the end.
+a = (input("enter the a" ))
+print (a,".")
+
+
+
+# Accept two integers and display their sum with a hash symbol (#) at the end.
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a+b,"#")
+
+
+
+#Request two integers and print their product with a plus sign (+) at the end.
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a*b,"+")
+
+
+
+#Calculate the division of two integers and display the result with an exclamation mark (!) at the end
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a/b,"!")
+
+
+
+#Find the remainder when one integer is divided by another, convert it to a string, and print it with an asterisk (*) at the end
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a%b,"*")
+
+
+
+#Take two integers as input, add them, and display the result with a tilde (~) at the end
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a+b,"~")
+
+
+
+#Accept two integers, subtract them, and print the result with a semicolon (;) at the end.
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a-b,";")
+
+
+
+#Request an integer from the user and display its square.
+a = int(input("enter the a "))
+b = int(input("enter the b "))
+print (a**b)
+
+
+
+#Create a program that doubles an integer and prints the result.
+a = int(input("enter the a "))
+print (a*2)
