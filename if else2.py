eb9cfc66678e0448dbf423fb54f141650d24a2a7
MustafaAgasiya <145117178+MustafaAgasiya@users.noreply.github.com>
2023-10-04 16:22:21 +0530

    Create if else2.py

---
+++ if else2.py
@@ -0,0 +1,40 @@
+num = int(input("enter the num "))
+if num > 0 :
+    print("number is positive")
+else :
+    print("number is negative")
+
+
+num = int(input("enter the num "))
+if num%2==0 :
+    print("number is even")
+else :
+    print("number is odd")
+
+
+num1=int(input("enter the num "))
+num2=int(input("enter the num2 "))
+if num1 > num2 :
+    print("num1")
+else :
+    print("num2")
+
+
+num1=int(input("enter the num "))
+num2=int(input("enter the num "))
+c = num1 + num2
+print(num1+num2)
+if c%2==0 :
+    print("number is even")
+else :
+    print("number is odd")
+
+
+num1=int(input("enter the num "))
+num2=int(input("enter the num "))
+num3=int(input("enter the num "))
+percent=(num1+num2+num3)/3
+if percent <=50:
+    print("above 50")
+else :
+    print("below 50")
