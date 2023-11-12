1d54734c3253f2c21f9929ad29762cd1644eda38
MustafaAgasiya <145117178+MustafaAgasiya@users.noreply.github.com>
2023-11-11 23:23:27 -0800

    Create practice que.py

---
+++ practice que.py
@@ -0,0 +1,14 @@
+a = input("enter the word ")
+if a.istitle() :
+    print("it is title")
+else :
+    print("it is not")
+
+
+
+
+sentence = "today is sunday"
+if sentence==sentence[::-1] :
+    print("it is sentence ")
+else :
+    ("it is not")
