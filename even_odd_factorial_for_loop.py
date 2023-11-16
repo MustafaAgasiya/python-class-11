b5e76f911ee523650bcfd80a60373ef1e059b884
MustafaAgasiya <145117178+MustafaAgasiya@users.noreply.github.com>
2023-11-16 16:01:29 +0530

    Create even_odd_factorial_for_loop.py

---
+++ even_odd_factorial_for_loop.py
@@ -0,0 +1,19 @@
+num=int(input("enter the num "))
+for i in range (1,num+1) :
+    if i%2==0 :
+        print(i)
+
+
+
+num=int(input("enter the num "))
+for i in range (1,num+1) :
+    if i%2!=0 :
+        print(i)
+
+
+
+num=int(input("enter the num "))
+factor=1
+for i in range (1,num+1) :
+    factor=factor*i
+print (factor)
